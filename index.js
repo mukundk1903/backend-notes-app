1cbfec6c81b76483a9288d610f8535e571cb6c99
mukundk1903 <mukundk712@gmail.com>
2024-11-03 18:30:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 //     })
 // );
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://notes-app-mu-olive.vercel.app"],
+    origin: "https://notes-app-mu-olive.vercel.app",
     credentials: true,
 };
 
